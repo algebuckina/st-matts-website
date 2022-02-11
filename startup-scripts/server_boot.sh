f213edd53a8f53aa2b8243d84fdeeec27c586f27
Algebuckina Design <algebuckina@icloud.com>
2022-02-12 10:57:41 +1100

    Update server_boot.sh

--- startup-scripts/server_boot.sh
+++ startup-scripts/server_boot.sh
@@ -5,8 +5,4 @@ sudo apt-get update
 sudo apt-get upgrade
 VBoxManage startvm "Church Website"
 
-VBoxManage controlvm "Church Website" poweroff | at 03:00
-VBoxManage clonevm "Church Website" --name="Website Backup" --register --snapshot="Snapshot 1"
-reboot
-
 exit 0
